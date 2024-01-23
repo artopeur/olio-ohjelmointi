1b9f4173530cdc29a23a702997a4d18bb36e16a8
artopeu <arto.peurasaari@gmail.com>
2024-01-24 00:14:58 +0200

    Graafinen versio pelistä valmis.

--- graph/mainwindow.cpp
+++ graph/mainwindow.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <iostream>
+#include <QLineEdit>
+#include <QCoreApplication>
+#include <Qdebug>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,10 +21,139 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::makeRandom() {
+    srand(time(0));
+    randomNumber=(rand() % 20) + 1;
+    QString strText = "The random value is: ";
+    ui->myTextBoxTest->setText(strText);
+}
 
 void MainWindow::on_StartButton_clicked()
 {
-    QString strText="Tähän tulee testiviesti.";
+    QString strText="Aloitetaan peli.\n";
+    makeRandom();
     ui->myTextBoxTest->setText(strText);
+}
+
+
+void MainWindow::on_Button0_clicked()
+{
+
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'0';
+    playerGuess = playerGuess+0;
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_text_guess_textChanged()
+{
+
+}
+
+void MainWindow::on_Button1_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'1';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button2_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'2';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button3_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'3';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button4_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'4';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button5_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'5';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button6_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'6';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button7_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'7';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button8_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'8';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_Button9_clicked()
+{
+    guess=ui->text_guess->toPlainText();
+
+    guess=guess+'9';
+    ui->text_guess->setText(guess);
+}
+
+
+void MainWindow::on_pushButtonCheck_clicked()
+{
+    QString text="";
+    ui->myTextBoxTest->setText("");
+    bool conversionCheck = false;
+    playerGuess = guess.toInt(&conversionCheck);
+
+    if(conversionCheck && playerGuess == randomNumber) {
+        text="Value was correct";
+        ui->myTextBoxTest->setText(text);
+    }
+    else if(conversionCheck && playerGuess < randomNumber){
+        text= "Value is larger.";
+    }
+    else if(conversionCheck) {
+        text = "value is lower";
+    }
+    else {
+        text = "Conversion failed.";
+    }
+    ui->myTextBoxTest->setText(text);
+    ui->text_guess->setText("");
 }
 
--- graph/mainwindow.h
+++ graph/mainwindow.h
@@ -12,14 +12,41 @@ QT_END_NAMESPACE
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QString guess="";
+    int randomNumber = 0;
+    int playerGuess = 0;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
+    void makeRandom();
 private slots:
 
     void on_StartButton_clicked();
+
+    void on_Button0_clicked();
+    void on_text_guess_textChanged();
+
+
+    void on_Button1_clicked();
+
+    void on_Button2_clicked();
+
+    void on_Button3_clicked();
+
+    void on_Button4_clicked();
+
+    void on_Button5_clicked();
+
+    void on_Button6_clicked();
+
+    void on_Button7_clicked();
+
+    void on_Button8_clicked();
+
+    void on_Button9_clicked();
+
+    void on_pushButtonCheck_clicked();
 
 private:
     Ui::MainWindow *ui;
