ba3275ec5085f6125dbfb52e76b2e44928d393ae
artopeu <arto.peurasaari@gmail.com>
2024-02-19 23:34:38 +0200

    Network test

---
+++ network_test/mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->btnTest, SIGNAL(clicked(bool)), this, SLOT(btnTest_clicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete manager;
+}
+
+void MainWindow::btnTest_clicked()
+{
+
+    url = "http://localhost:3000/login";
+    checkSite(url);
+}
+
+void MainWindow::readData(QNetworkReply* reply) {
+    qDebug()<<"Received signal, went to slot";
+    qDebug()<<reply->readAll();
+    //qDebug() << reply;
+}
+void MainWindow::checkSite(QString url) {
+    QUrl qrl(url);
+    manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(readData(*reply)));
+    QNetworkRequest request(qrl);
+
+
+    manager->get(request);
+    //QNetworkRequest request(url);
+    qDebug()<<"request sent.";
+}
---
+++ network_test/mainwindow.h
@@ -0,0 +1,37 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QCoreApplication>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    QNetworkAccessManager *manager;
+    QString url;
+    void checkSite(QString url);
+
+private slots:
+    void btnTest_clicked();
+    void readData(QNetworkReply*);
+
+private:
+    Ui::MainWindow *ui;
+
+signals:
+    void finished();
+};
+#endif // MAINWINDOW_H
